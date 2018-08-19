14ed0062879282f10d81ff85dbdba0edb7d2baa1
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 13:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ veto(){return "No, I must disagree"}
 
 approve(){return "You can do that!"}
 
-doCharity(){returns I like to help people.}
+doCharity(){return "I like to help people."}
 
-releasePressStatement — returns You will see great things from Scuber.
+releasePressStatement(){returns You will see great things from Scuber.}
 
 sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
 }
