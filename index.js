6099b821333b6de313470bc5299b29525fd2161d
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 13:48:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ doCharity(){return "I like to help people."}
 
 releasePressStatement(){return "You will see great things from Scuber."}
 
-sayHi(){returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.}
+sayHi(){return `Hi, my name is ${this.name}. I am from <homestate>, and I was trained in ${this.training}.`}
 }
